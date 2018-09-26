978c729809bf1738302548dae83ba59b362e3d39
Chris <chrismc@austin.rr.com>
2018-09-26 08:25:21 -0500

    This is Feature 2
    * Started my second feature
    
    * Added to my second feature
    
    * some more work on feature 2

--- GitTest/ClassA.swift
+++ GitTest/ClassA.swift
@@ -3,4 +3,9 @@ class aClass {
     func myNewFeature() {
         
     }
+    
+    func myOtherFeature() {
+        let anImportantVariable: Int = 0
+        print("This is important: \(anImportantVariable)")
+    }
 }
